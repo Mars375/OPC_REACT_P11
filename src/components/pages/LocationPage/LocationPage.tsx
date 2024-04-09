9f6433f4ef7e16818f23f4068ba2fb4944c1b8b0
Mars375 <loic_2610@hotmail.fr>
2024-04-09 08:36:19 +0200

    feat: redirect if route doesnt match

--- src/components/pages/LocationPage/LocationPage.tsx
+++ src/components/pages/LocationPage/LocationPage.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import DefaultTemplate from "../../templates/DefaultTemplate/DefaultTemplate";
+import LocationsContext from "../../../contexts/LocationsContext";
 
 export default function LocationPage() {
+	const { id } = useParams<{ id: string }>();
+	const locationsContext = useContext(LocationsContext);
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (!locationsContext) {
+			throw new Error("LocationsContext not found");
+		}
+
+		const location = locationsContext.locations.find(
+			(location) => location.id === id
+		);
+
+		console.log(location);
+		if (!location) {
+			navigate("/404");
+		}
+	}, [id, locationsContext, navigate]);
+
 	return (
 		<DefaultTemplate>
 			<h1>Location page</h1>
